038b9a079f721737a0aa5469352e70da0ceb64f6
Hakan Berke İçellioğlu <hakanicellioglu@gmail.com>
2025-07-05 11:35:36 +0300

    fix: redirection corrected

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['user_id'])) {
 ?>
 <nav id="mainNavbar" class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="index">Teklif Pro</a>
+        <a class="navbar-brand" href="dashboard">Teklif Pro</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
